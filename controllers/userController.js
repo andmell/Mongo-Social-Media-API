2cbc7eb59fc884be93e6e34206d31483d1f02340
ahmell <andrewmell10@gmail.com>
2023-10-22 16:52:31 -0500

    added and tested all user routes.

---
+++ controllers/userController.js
@@ -0,0 +1,59 @@
+const User = require("../models/User");
+
+module.exports = {
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params._id }).select(
+        "-__v"
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const dbUserData = await User.create(req.body);
+      res.json(dbUserData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params._id },
+        { ...req.body },
+        { new: true }
+      );
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const result = await User.findOneAndDelete({ _id: req.params._id });
+      res.status(200).json(result);
+      console.log(`Deleted: ${result}`);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
